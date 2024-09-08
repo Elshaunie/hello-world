49828145b4204a9e4df2b5e343ab9f39f9d54e94
Elshaunie <elshaunie@gmail.com>
2024-09-08 13:41:24 +0200

    Add files via upload

---
+++ Task2.js
@@ -0,0 +1,15 @@
+function task2(Sally){
+    // Hello Sally
+}
+
+module.exports = {task2};
+function task2(Tshepo){
+    // Hello Tshepo
+}
+
+module.exports = {task2};
+function task2(){
+    // Hello Friend!
+}
+
+module.exports = {task2};
